0d306e0ef92c860836639e4da52745f56b5b7b79
FP\pstolka <pstolka@future-processing.com>
2022-02-16 05:31:09 +0100

    refresh

---
+++ Exercises/Lib.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercises
+{
+    public static class Lib
+    {
+        public static void ShowExercise()
+        {
+            var exercises = Exercises();
+            var index = 1;
+
+            foreach (var item in exercises)
+            {
+                var msg = $"{index++}. {item}";
+                Console.WriteLine(msg);
+                Console.WriteLine();
+            }
+        }
+
+        static List<string> Exercises()
+        {
+            return new List<string>
+            {
+                "Zamień tło całej niebieskiej linii tak, żeby niebieski pojawiał się tylko pod tekstem.",
+                "Wypchnij zmiany do repozytorium..."
+            };
+        }
+    }
+
+}
